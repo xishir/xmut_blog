213d296a95a909fcf9d8db28d7fa29db2258b2ac
xishir <daoyuan615958739@qq.com>
2018-01-15 11:23:38 +0800

    change database

--- src/main/java/com/xmut/dao/impl/UserInfoImpl.java
+++ src/main/java/com/xmut/dao/impl/UserInfoImpl.java
@@ -23,12 +23,12 @@ public class UserInfoImpl implements UserInfoMapper {
 
     @Override
     public void createUser(String tel,String pwd) {
-        jdbcTemplate.update("INSERT INTO blog.tp_user(tel,password,nickname,secret) VALUES (?,md5(?),?,'')",tel,pwd,tel);
+        jdbcTemplate.update("INSERT INTO keshe.tp_user(tel,password,nickname,secret) VALUES (?,md5(?),?,'')",tel,pwd,tel);
     }
 
     @Override
     public UserInfo getUser(Integer id) {
-        List<UserInfo> userList = jdbcTemplate.query("select tel,nickname,password FROM blog.tp_user WHERE user_id = ?",new Object[]{id},new BeanPropertyRowMapper(UserInfo.class));
+        List<UserInfo> userList = jdbcTemplate.query("select tel,nickname,password FROM keshe.tp_user WHERE user_id = ?",new Object[]{id},new BeanPropertyRowMapper(UserInfo.class));
         if(userList != null && userList.size() > 0){
             UserInfo user = userList.get(0);
             return user;
@@ -39,11 +39,11 @@ public class UserInfoImpl implements UserInfoMapper {
 
     @Override
     public void updateUser(String user_id, String nickName) {
-        jdbcTemplate.update("UPDATE blog.tp_user SET nickname = ? WHERE user_id = ?",nickName,user_id);
+        jdbcTemplate.update("UPDATE keshe.tp_user SET nickname = ? WHERE user_id = ?",nickName,user_id);
     }
 
     @Override
     public void deleteUserByUserId(Integer id) {
-        jdbcTemplate.update("DELETE FROM blog.tp_user WHERE user_id = ?",id);
+        jdbcTemplate.update("DELETE FROM keshe.tp_user WHERE user_id = ?",id);
     }
 }
