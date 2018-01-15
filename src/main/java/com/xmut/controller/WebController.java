ce8717f7c57abd3012ee4fe70b6d9d1267c074d4
xishir <daoyuan615958739@qq.com>
2018-01-15 22:50:40 +0800

    add bootstrap

--- src/main/java/com/xmut/controller/WebController.java
+++ src/main/java/com/xmut/controller/WebController.java
@@ -1,17 +1,24 @@
 package com.xmut.controller;
 
 import org.springframework.stereotype.Controller;  
-import org.springframework.ui.ModelMap;  
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;  
   
 @Controller  
-@RequestMapping("/web")  
+@RequestMapping("/")  
 public class WebController {  
       
     @RequestMapping  
     public String index(ModelMap map){  
         map.put("title", "HelloWorld");  
         return "index";  
+    }  
+    
+    @RequestMapping("/blog")
+    public String blog(ModelMap map){  
+        map.put("title", "HelloWorld");  
+        return "blog";  
     }  
       
 }  
---
+++ src/main/resources/templates/footer.ftl
@@ -0,0 +1,17 @@
+<footer class="blog-footer">
+	<p>
+		Blog template built for <a href="http://getbootstrap.com">Bootstrap</a>
+		by <a href="https://twitter.com/mdo">@mdo</a>.
+	</p>
+	<p>
+		<a href="#">Back to top</a>
+	</p>
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="/js/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
+<script src="/bootstrap/js/bootstrap.min.js"></script>
