9537401c5b0b62ecd418ac5babb26b75a11f72c4
Xishir <daoyuan615958739@qq.com>
2018-01-19 16:48:34 +0800

    Update XssUtil.java

--- src/main/java/com/xmut/util/XssUtil.java
+++ src/main/java/com/xmut/util/XssUtil.java
@@ -3,7 +3,20 @@ package com.xmut.util;
 public class XssUtil {
 	public static String cleanXSS(String value) {
         //You'll need to remove the spaces from the html entities below
-		value = value.substring(0,150);
+		if(value.length()>151)
+		{
+			value = value.substring(0,150);
+		}
+		value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
+		value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
+		value = value.replaceAll("'", "& #39;");
+		value = value.replaceAll("eval\\((.*)\\)", "");
+		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+		value = value.replaceAll("script", "");
+		return value;
+	}
+	public static String cleanXSS2(String value) {
+        //You'll need to remove the spaces from the html entities below
 		value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
 		value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
 		value = value.replaceAll("'", "& #39;");
